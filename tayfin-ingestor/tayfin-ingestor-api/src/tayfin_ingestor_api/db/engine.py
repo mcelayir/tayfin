f728925b7e63a019befef42b652aa4d6ed5f1a2c
M Celayir <muratcancelayir@gmail.com>
2026-02-09 21:55:20 +0100

    api(db): accept POSTGRES_* env vars as fallbacks for DB_* settings

--- tayfin-ingestor/tayfin-ingestor-api/src/tayfin_ingestor_api/db/engine.py
+++ tayfin-ingestor/tayfin-ingestor-api/src/tayfin_ingestor_api/db/engine.py
@@ -4,11 +4,11 @@ from sqlalchemy import create_engine
 
 def get_engine():
     # Read DB connection info from env vars
-    host = os.getenv('DB_HOST', 'localhost')
-    port = os.getenv('DB_PORT', '5432')
-    db = os.getenv('DB_NAME', 'tayfin')
-    user = os.getenv('DB_USER', 'tayfin')
-    password = os.getenv('DB_PASS', '')
+    host = os.getenv('DB_HOST', os.getenv('POSTGRES_HOST', 'localhost'))
+    port = os.getenv('DB_PORT', os.getenv('POSTGRES_PORT', '5432'))
+    db = os.getenv('DB_NAME', os.getenv('POSTGRES_DB', 'tayfin'))
+    user = os.getenv('DB_USER', os.getenv('POSTGRES_USER', 'tayfin'))
+    password = os.getenv('DB_PASS', os.getenv('POSTGRES_PASSWORD', ''))
 
     url = f'postgresql://{user}:{password}@{host}:{port}/{db}'
     engine = create_engine(url, future=True)
